393919aa9b51fab49d0a9fc4f79500bb1f48e6a0
carlos <115598773+maninthehood@users.noreply.github.com>
2023-07-11 15:09:24 -0400

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
     return (
         <>
             <section class="bg-white dark:bg-gray-900">
-                <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+                <div class="py-8 px-4 mb-56 max-w-screen-xl sm:py-16 lg:px-6">
                     <div class="mx-auto max-w-screen-sm text-center">
                         <h2 class="mb-4 text-4xl tracking-tight font-extrabold leading-tight text-gray-900 dark:text-white">Here are `the name of the hospital`. We provide just the care you need.</h2>
                         <p class="mb-6 font-light text-gray-500 dark:text-gray-400 md:text-lg">Check out our awesome <a href="">Staff</a></p>
