aa4c083c44b1771dfc44f75b1734a77cdb222947
carlos <115598773+maninthehood@users.noreply.github.com>
2023-07-11 15:11:00 -0400

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
 		<>
 
 
-			<section class="bg-center bg-no-repeat bg-[url('https://www.novaurology.com/wp-content/uploads/2021/04/MYP_Summit-PR-3091-optimized.jpg')] bg-gray-700 bg-blend-multiply">
+			<section class="bg-center bg-fixed bg-no-repeat bg-[url('https://www.novaurology.com/wp-content/uploads/2021/04/MYP_Summit-PR-3091-optimized.jpg')] bg-gray-700 bg-blend-multiply">
 				<div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
 					<h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">I invest in Building Beautiful Websites</h1>
 					<p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at <code>`Hospital Name`</code> we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
